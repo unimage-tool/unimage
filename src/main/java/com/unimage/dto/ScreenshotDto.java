dc699ad122e0705fa359e054a90ae9f40baa59ba
flffkd12 <flffkd12@gmail.com>
2025-01-15 22:05:04 +0900

    docs: ScreenshotDto 클래스의 주석 Javadoc 스타일로 수정
    * 클래스 설명 추가
    * fileName, filePath, date 필드에 대한 주석 수정
    * 기능이 따로 없어 불필요한 Getter/Setter 제거를 위해 @Data 제거
    * 필드가 immutable하고 final하므로 접근 제어자 수정

--- src/main/java/com/unimage/dto/ScreenshotDto.java
+++ src/main/java/com/unimage/dto/ScreenshotDto.java
@@ -1,12 +1,25 @@
 package com.unimage.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
+/**
+ * 스크린샷 정보를 담는 DTO입니다.
+ */
 @AllArgsConstructor
 public class ScreenshotDto {
-    private final String fileName; // 생성시 저장한 파일 명 ex) "logo.png"
-    private final String filePath; // 생성시 저장한 경로 ex) "C:/Server/Unimage~~"
-    private final String date; // 생성 날짜 ex) "2024-09-23"
+    /**
+     * 생성 시 저장한 파일명
+     * ex) "logo.png"
+     */
+    public final String fileName;
+    /**
+     * 생성 시 저장한 파일경로
+     * ex) "C:/Server/Unimage~~"
+     */
+    public final String filePath;
+    /**
+     * 생성 시 저장한 날짜
+     * ex) "2024-09-23"
+     */
+    public final String date;
 }
