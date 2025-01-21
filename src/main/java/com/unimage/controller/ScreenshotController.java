5487e979abe58b60b8247d21dbec2e69c7a9e334
flffkd12 <flffkd12@gmail.com>
2025-01-21 23:37:21 +0900

    docs: 코드 수정에 따른 Javadoc 수정
    * ApiResponse 클래스의 메서드 Javadoc 수정
    * 이전의 ScreenshotDto의 파일 저장 경로 제거 관련 Javadoc 수정

--- src/main/java/com/unimage/controller/ScreenshotController.java
+++ src/main/java/com/unimage/controller/ScreenshotController.java
@@ -121,8 +121,8 @@ public class ScreenshotController {
    * 저장된 파일들을 날짜순으로 전부 불러옵니다.
    *
    * @param email 사용자 고유 식별자
-   * @return {@link ApiResponse}를 통해 성공 시 200 OK와 {@link ScreenshotDto} 리스트 반환, 실패 시 상태 코드와 에러 메시지
-   * 반환 {@link ScreenshotDto}는 스크린샷의 파일명, 저장 경로, 생성일자를 포함
+   * @return {@link ApiResponse}를 통해 성공 시 200 OK와 {@link ScreenshotDto<List>} 반환, 실패 시 상태코드와 에러 메시지
+   * 반환 {@link ScreenshotDto}는 스크린샷의 파일명, 생성일자를 포함
    */
   @GetMapping("/all")
   public ApiResponse<List<ScreenshotDto>> getAllScreenshots(
@@ -148,7 +148,7 @@ public class ScreenshotController {
    * @param email    사용자 고유 식별자
    * @param filename 반환할 파일의 파일명
    * @return {@link ApiResponse}를 통해 성공 시 200 OK와 {@link ScreenshotDto} 반환, 실패 시 상태 코드와 에러 메시지 반환
-   * {@link ScreenshotDto}는 스크린샷의 파일명, 저장 경로, 생성일자를 포함
+   * {@link ScreenshotDto}는 스크린샷의 파일명, 생성일자를 포함
    */
   @GetMapping("/{filename}")
   public ApiResponse<ScreenshotDto> getScreenshot(
--- src/main/java/com/unimage/dto/ApiResponse.java
+++ src/main/java/com/unimage/dto/ApiResponse.java
@@ -16,8 +16,9 @@ public class ApiResponse<T> extends ResponseEntity<T> {
   /**
    * ApiResponse 생성자
    *
+   * @param body    응답 데이터
    * @param message 응답 메시지
-   * @param data    응답 데이터
+   * @param status  응답 상태코드
    */
   private ApiResponse(T body, String message, HttpStatus status) {
     super(body, status);
@@ -25,16 +26,22 @@ public class ApiResponse<T> extends ResponseEntity<T> {
   }
 
   /**
-   * 성공적인 응답을 생성하는 static 메서드
+   * 반환될 데이터와 함께 성공적인 응답을 생성하는 static 메서드
    *
    * @param <T>  응답 데이터 유형
-   * @param data 응답 데이터
+   * @param body 응답 데이터
    * @return 성공적인 응답 객체
    */
   public static <T> ApiResponse<T> success(T body) {
     return new ApiResponse<>(body, null, HttpStatus.OK);
   }
 
+  /**
+   * 반환될 데이터 없이 성공적인 응답을 생성하는 static 메서드
+   *
+   * @param <T> 응답 데이터 유형
+   * @return 성공적인 응답 객체
+   */
   public static <T> ApiResponse<T> success() {
     return new ApiResponse<>(null, null, HttpStatus.OK);
   }
@@ -42,7 +49,9 @@ public class ApiResponse<T> extends ResponseEntity<T> {
   /**
    * 실패 응답을 생성하는 static 메서드
    *
+   * @param <T>     응답 데이터 유형
    * @param message 실패 메시지
+   * @param status  응답 상태코드
    * @return 실패 응답 객체
    */
   public static <T> ApiResponse<T> error(String message, HttpStatus status) {
