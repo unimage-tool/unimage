14824a985512b1983611a94e1187bbb935cfc701
flffkd12 <flffkd12@gmail.com>
2025-02-10 02:59:18 +0900

    feat: 로그인 후 승인된 Redirection Uri로 이동

--- src/main/java/com/unimage/config/SecurityConfig.java
+++ src/main/java/com/unimage/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.unimage.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,7 +23,8 @@ public class SecurityConfig {
             .requestMatchers("/").authenticated()
             .anyRequest().denyAll()
         )
-        .oauth2Login(Customizer.withDefaults());
+        .oauth2Login(oauth -> oauth.successHandler((request, response, authentication) -> {
+        }));
 
     return http.build();
   }
