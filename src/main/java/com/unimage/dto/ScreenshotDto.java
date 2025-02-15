3610ec8b4686763d260775d482737fd377319fa8
flffkd12 <flffkd12@gmail.com>
2025-02-15 16:58:18 +0900

    chore: PR 목적과 맞지 않는 코드 제거

--- src/main/java/com/unimage/dto/ScreenshotDto.java
+++ src/main/java/com/unimage/dto/ScreenshotDto.java
@@ -1,6 +1,5 @@
 package com.unimage.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 /** 스크린샷 정보를 담는 DTO입니다. */
@@ -8,6 +7,5 @@ import lombok.AllArgsConstructor;
 public class ScreenshotDto {
 
   /** 생성 시 저장한 파일명 ex) "logo.png" */
-  @JsonProperty
   public final String fileName;
 }
