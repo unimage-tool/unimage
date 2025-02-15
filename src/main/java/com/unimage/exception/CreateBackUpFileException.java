601ee0b90d1c1c732111cc4650b433abbc18e481
flffkd12 <flffkd12@gmail.com>
2025-02-15 16:53:58 +0900

    chore: 커스텀 예외를 사용하는 타입 명시

--- src/main/java/com/unimage/exception/CreateBackUpFileException.java
+++ src/main/java/com/unimage/exception/CreateBackUpFileException.java
@@ -2,6 +2,13 @@ package com.unimage.exception;
 
 public class CreateBackUpFileException extends Exception {
 
+  public enum ExceptionType {
+    UNSUPPORTED_OPERATION_EXCEPTION,
+    SOCKET_EXCEPTION,
+    INTERRUPTED_BY_TIMEOUT_EXCEPTION,
+    IO_EXCEPTION
+  }
+
   public CreateBackUpFileException(String message, Throwable cause) {
     super(message, cause);
   }
